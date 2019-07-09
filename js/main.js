b7c7235fb93727bd6fa0d5d58975ae84814d0a7d
Vadim Valunskii <dvvv09@gmail.com>
2019-07-09 05:43:10 +0300

    Initial commit: plug jQuery in

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ // best-practice скрипт будет работать только когда страница полностью загрузится
+
+	$('#button').on('click', function(){
+
+		alert('Hello, it is jQuery!');
+
+	});
+
+});
